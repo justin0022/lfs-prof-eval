2a5cfff65be12ebcf02bce094b8521391a205269
Justin Lee <justin.lee@ubc.ca>
2017-03-31 13:24:43 -0700

    Successfully cleaned data

--- parse.py
+++ parse.py
@@ -5,25 +5,32 @@ def parseData():
     # need to keep col 3,4,7,8,9,10,11,12,13,16,17,18,19,22,23,24,25
     with open('data.csv', 'rb') as csvfile:
         f = csv.reader((line.replace('\0','') for line in csvfile), delimiter="\t")
-        for row in f:
-            print row[3]
-            print row[4]
-            print row[7]
-            print row[8]
-            print row[9]
-            print row[10]
-            print row[11]
-            print row[12]
-            print row[13]
-            print row[17]
-            print row[18]
-            print row[19]
-            print row[22]
-            print row[23]
-            print row[24]
-            print row[25]
-            break
-            
+        header_row = next(f)
+        for line in f:
+            lineDict = {header_row[3]: line[3], 
+                        header_row[4]: line[4],
+                        header_row[7]: line[7],
+                        header_row[8]: line[8],
+                        header_row[9]: line[9],
+                        header_row[10]: line[10],
+                        header_row[11]: line[11],
+                        header_row[12]: line[12],
+                        header_row[13]: line[13],
+                        header_row[16]: line[16],
+                        header_row[17]: line[17],
+                        header_row[18]: line[18],
+                        header_row[18]: line[18],
+                        header_row[18]: line[18],
+                        header_row[19]: line[19],
+                        header_row[22]: line[22],
+                        header_row[23]: line[23],
+                        header_row[24]: line[24],
+                        header_row[25]: line[25]}
+            # filter out any rows that have fewer than 5 submissions and don't have question code IUMI06
+            if int(lineDict['Submissions']) > 5 and lineDict['Question Code'] == 'IUMI06':
+                filteredList.append(lineDict)
+        return filteredList
+
 def main():
     parseData()
  
